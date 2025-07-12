4679e089b5be09ef10f66da6faa322131f024d39
ithomm11 <121775774+ithomm11@users.noreply.github.com>
2025-07-12 16:41:33 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello from GitHub Actions!" << std::endl;
+    std::cout << "This is a C++ project compiled on Windows Server 2022." << std::endl;
+    return 0;
+}
